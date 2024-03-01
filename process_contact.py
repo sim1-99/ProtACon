Add contact map normalization and fix divide by zero error

process_contact.py
@@ -11,6 +11,7 @@ __author__ = 'Simone Chiarella'
 __email__ = 'simone.chiarella@studio.unibo.it'
 
 from modules.contact import binarize_contact_map, generate_distance_map
+from modules.utils import normalize_array
 import numpy as np
 
 
@@ -27,7 +28,7 @@ def main(CA_Atoms: tuple) -> (np.ndarray, np.ndarray, np.ndarray):
     distance_map : np.ndarray
         it shows the distance - expressed in Angstroms - between each couple of
         amino acids in the peptide chain
-    contact_map : np.ndarray
+    norm_contact_map : np.ndarray
         it shows how much each amino acid is close to all the others, in a
         scale between 0 and 1
     binary_contact_map : np.ndarray
@@ -35,11 +36,18 @@ def main(CA_Atoms: tuple) -> (np.ndarray, np.ndarray, np.ndarray):
 
     """
     distance_map = generate_distance_map(CA_Atoms)
-    contact_map = np.array(1/distance_map)
+    distance_map_copy = distance_map.copy()
+
+    # set array diagonal to 0 to avoid divide by 0 error
+    distance_map_copy[distance_map_copy == 0.] = np.nan
+    contact_map = np.array(1/distance_map_copy)
+    del distance_map_copy
+
+    norm_contact_map = normalize_array(contact_map)
 
     distance_cutoff = 8.0
     position_cutoff = 6
     binary_contact_map = binarize_contact_map(
         distance_map, distance_cutoff, position_cutoff)
 
-    return distance_map, contact_map, binary_contact_map
+    return distance_map, norm_contact_map, binary_contact_map