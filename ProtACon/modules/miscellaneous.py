7337ff12a5ac2ac2d7bf3eb2fb122ca2bca9cc46
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-07-01 14:20:02 +0200

    increase the output-volume of get_AA_features_dataframe, adding local_flexibility, protein_reference_point to this script

--- ProtACon/modules/miscellaneous.py
+++ ProtACon/modules/miscellaneous.py
@@ -12,11 +12,13 @@ This module contains:
     - funtions for extracting information from ProtBert and from PDB objects
 """
 
-__author__ = 'Simone Chiarella'
-__email__ = 'simone.chiarella@studio.unibo.it'
+__author__ = 'Simone Chiarella, Renato Eliasy'
+__email__ = 'simone.chiarella@studio.unibo.it, renato.eliasy@studio.unibo.it'
 
 import logging
-
+from Bio.SeqUtils.ProtParamData import Flex, kd, hw, em, ja, DIWV
+from Bio.SeqUtils.IsoelectricPoint import IsoelectricPoint
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
 from Bio.PDB.Structure import Structure
 from transformers import BertModel, BertTokenizer
 import torch
@@ -69,7 +71,7 @@ dict_3_to_1 = {
     "VAL": "V"
 }
 
-dict_hydropathy_kyte_doolittle = {
+dict_hydropathy_kyte_doolittle = {  # the same as kd of Bio.SeqUtils.ProtParamData
     "A": 1.8,
     "R": -4.5,
     "N": -3.5,
@@ -95,24 +97,24 @@ dict_hydropathy_kyte_doolittle = {
 dict_AA_charge = {
     "A": 0,   # (neutral)
     "R": 1,   # (positively charged)
-    "N": 0,   
+    "N": 0,
     "D": -1,  # (negatively charged)
-    "C": 0,   
-    "Q": 0,   
-    "E": -1,  
-    "G": 0,   
-    "H": 0,   
-    "I": 0,   
-    "L": 0,   
-    "K": 1,   
-    "M": 0,   
-    "F": 0,   
-    "P": 0,   
-    "S": 0,   
-    "T": 0,   
-    "W": 0,   
-    "Y": 0,   
-    "V": 0    
+    "C": 0,
+    "Q": 0,
+    "E": -1,
+    "G": 0,
+    "H": 0,
+    "I": 0,
+    "L": 0,
+    "K": 1,
+    "M": 0,
+    "F": 0,
+    "P": 0,
+    "S": 0,
+    "T": 0,
+    "W": 0,
+    "Y": 0,
+    "V": 0
 }
 
 # Values are approximate and in cubic angstroms (Å^3)
@@ -140,31 +142,31 @@ dict_AA_volumes = {
 }
 
 dict_AA_PH = {
-        'A': 6.01,
-        'R': 10.76,
-        'N': 5.41,
-        'D': 2.77,
-        'C': 5.07,
-        'Q': 5.65,
-        'E': 3.22,
-        'G': 5.97,
-        'H': 7.59,
-        'I': 6.02,
-        'L': 5.98,
-        'K': 9.74,
-        'M': 5.74,
-        'F': 5.48,
-        'P': 6.30,
-        'S': 5.68,
-        'T': 5.60,
-        'W': 5.89,
-        'Y': 5.66,
-        'V': 5.96
-    }
+    'A': 6.01,
+    'R': 10.76,
+    'N': 5.41,
+    'D': 2.77,
+    'C': 5.07,
+    'Q': 5.65,
+    'E': 3.22,
+    'G': 5.97,
+    'H': 7.59,
+    'I': 6.02,
+    'L': 5.98,
+    'K': 9.74,
+    'M': 5.74,
+    'F': 5.48,
+    'P': 6.30,
+    'S': 5.68,
+    'T': 5.60,
+    'W': 5.89,
+    'Y': 5.66,
+    'V': 5.96
+}
 
-dict_charge_density_Rgroups ={ # considering only the volume occupied by the Rgroup
+dict_charge_density_Rgroups = {  # considering only the volume occupied by the Rgroup
     'A': 0.0,
-    'R': 8.8261253309797, # provided in mA/Å^3
+    'R': 8.8261253309797,  # provided in mA/Å^3
     'N': 0.0,
     'D': -19.607843137254903,
     'C': 0.0,
@@ -185,9 +187,9 @@ dict_charge_density_Rgroups ={ # considering only the volume occupied by the Rgr
     'V': 0.0
 }
 
-dict_charge_density={ # considering the whole volume of the amino acid
+dict_charge_density = {  # considering the whole volume of the amino acid
     'A': 0.0,
-    'R': 5.767, # provided in mA/Å^3
+    'R': 5.767,  # provided in mA/Å^3
     'N': 0.0,
     'D': -9.0009,
     'C': 0.0,
@@ -207,6 +209,7 @@ dict_charge_density={ # considering the whole volume of the amino acid
     'Y': 0.0,
     'V': 0.0
 }
+
 
 class CA_Atom:
     """A class to represent CA atoms of amino acids."""
@@ -234,7 +237,7 @@ class CA_Atom:
         coords : list[float]
             x-, y- and z- coordinates of the CA atom of the amino acid
         hydropathy : float
-            hydropathy value by Kyte and Doolittle hydrophobicity(+)/hydrophilicity(-) 
+            hydropathy value by Kyte and Doolittle hydrophobicity(+)/hydrophilicity(-)
         volume : float
             volume of the AA in cubic angstroms (Å^3)
         charge_density : float
@@ -254,7 +257,8 @@ class CA_Atom:
         self.charge_density = charge_density
         self.Rcharge_density = Rcharge_density
         self.charge = charge_density*volume
-        self.aa_ph = aa_ph        
+        self.aa_ph = aa_ph
+
 
 def extract_CA_Atoms(
     structure: Structure
@@ -287,12 +291,16 @@ def extract_CA_Atoms(
                     name=dict_3_to_1[residue.get_resname()],
                     idx=residue_idx,
                     coords=atom.get_coord(),
-                    hydropathy=dict_hydropathy_kyte_doolittle[dict_3_to_1[residue.get_resname()]],
-                    charge_density=dict_charge_density[dict_3_to_1[residue.get_resname()]],
+                    hydropathy=dict_hydropathy_kyte_doolittle[dict_3_to_1[residue.get_resname(  # change with Bio.SeqUtils.ProtParamData
+                    )]],
+                    charge_density=dict_charge_density[dict_3_to_1[residue.get_resname(
+                    )]],
                     volume=dict_AA_volumes[dict_3_to_1[residue.get_resname()]],
+                    # change with Bio.SeqUtils.ProtParamData iso ph
                     aa_ph=dict_AA_PH[dict_3_to_1[residue.get_resname()]],
                     Rcharge_density=dict_charge_density_Rgroups[dict_3_to_1[residue.get_resname()]])
-                    )
+
+                )
                 break
             elif atom.get_name() == "CA":
                 logging.warning(" Found and discarded ligand in position: "
@@ -301,9 +309,77 @@ def extract_CA_Atoms(
 
     return CA_Atoms_tuple
 
+
+def local_flexibility(protein_sequence: str  # the protein sequence, possibly without space char
+                      ) -> tuple[float, ...]:  # return the specific calcula for a window of size 9
+    """
+    Since the biopython repository doesn't solve the issue on the flexibility,
+    we have to correct and reintroduce the method from scratches
+    we handle the borders putting them to 0
+
+    Parameters
+    ----------
+    protein_sequence : str
+        sequence of amino acids
+
+    Returns
+    -------
+    flexibility : tuple(float, ...)
+        tuple containing the flexibility of the amino acids in the protein sequence
+    """
+    protein_sequence_ns = str(protein_sequence.replace(' ', '')).upper()
+    flexibilities = Flex
+    window_size = 9
+    weights = [0.25, 0.4375, 0.625, 0.8125, 1]
+    scores = []
+
+    for i in range(len(protein_sequence_ns) - window_size + 1):
+        subsequence = protein_sequence_ns[i: i + window_size]
+        score = 0.0
+
+        for j in range(window_size // 2):
+            front = subsequence[j]
+            back = subsequence[window_size - j - 1]
+            score += (flexibilities[front] + flexibilities[back]) * weights[j]
+
+        middle = subsequence[window_size // 2]
+        score += flexibilities[middle]
+
+        scores.append(score / 5.25)
+
+    # since the first and last 4 are not computed in the score count:
+    border_handle = [0, 0, 0, 0]
+    AA_flexibilities = border_handle + scores + border_handle
+
+    if len(AA_flexibilities) != len(protein_sequence_ns):
+        raise Exception('something wrong in the calculus of flexibilities')
+    else:
+        return tuple(AA_flexibilities)
+
+
+def secondary_structure_index(amminoacid_name: str) -> int:
+    """
+    Return the index of the secondary structure of the amminoacid
+    """
+    is_helix = 'VIYFWL'
+    is_turn = 'NPGS'
+    is_sheet = 'EMAL'
+    if len(amminoacid_name) != 1:
+        raise ValueError('The name of amminoacids must be a one-value-letter')
+    else:
+        if amminoacid_name in is_helix:
+            return 1
+        elif amminoacid_name in is_turn:
+            return 2
+        elif amminoacid_name in is_sheet:
+            return 3
+        else:
+            return 0  # for undefined structure
+
+
 def get_AA_features_dataframe(
     CA_Atoms: tuple[CA_Atom, ...]
-)-> pd.DataFrame:
+) -> pd.DataFrame:
     """
     Build a DataFrame containing the features of the amino acids.
 
@@ -327,6 +403,9 @@ def get_AA_features_dataframe(
     AA_features_dataframe : pd.DataFrame
 
     """
+    protein_sequence_no_space = ''.join(AA.name for AA in CA_Atoms)
+    flexibilities = local_flexibility(protein_sequence_no_space)
+
     data = {                                        # dictionary to build the DataFrame
         'AA_Name': [AA.name for AA in CA_Atoms],
         'AA_Coords': [AA.coords for AA in CA_Atoms],
@@ -335,13 +414,68 @@ def get_AA_features_dataframe(
         'AA_Charge_Density': [AA.charge_density for AA in CA_Atoms],
         'AA_Rcharge_density': [AA.Rcharge_density for AA in CA_Atoms],
         'AA_Charge': [AA.charge for AA in CA_Atoms],
-        'AA_PH': [AA.aa_ph for AA in CA_Atoms]
+        'AA_PH': [AA.aa_ph for AA in CA_Atoms],
+        'AA_isoPH': [IsoelectricPoint(AA.name).pi() for AA in CA_Atoms],
+        'AA_Hydrophilicity': [hw[AA.name] for AA in CA_Atoms],
+        'AA_Surface_accessibility': [em[AA.name] for AA in CA_Atoms],
+        'AA_ja_transfer_energy_scale': [ja[AA.name] for AA in CA_Atoms],
+        'AA_self_Flex': [Flex[AA.name] for AA in CA_Atoms],
+        'AA_local_flexibility': [AA_flex for AA_flex in flexibilities],
+        'AA_secondary_structure': [secondary_structure_index(AA.name) for AA in CA_Atoms]
+
+
     }
 
-    AA_features_dataframe = pd.DataFrame(data, index=[AA.idx for AA in CA_Atoms])
+    AA_features_dataframe = pd.DataFrame(
+        data, index=[AA.idx for AA in CA_Atoms])
 
-    return AA_features_dataframe    
-        
+    return AA_features_dataframe
+
+
+def protein_reference_point(protein_sequence: str
+                            ) -> dict:
+    """
+    Calculate the reference point of the specific protein, get information from ProteinAnalysis of BioPython libs
+    Parameters
+    ----------
+    protein_sequence : str
+        sequence of amino acids
+
+    Returns
+    -------
+    reference_point : dict
+        dictionary containing the reference point of the protein:
+        - 'molecular_weight': float
+        - 'aromaticity': float
+        - 'instability_index': float
+        - 'flexibility': float
+        - 'isoelectric_point': float
+        - 'mono isotopic' : bool
+        - 'gravy' : float
+        - 'secondary_structure_inclination' : dict of floats
+
+
+    """
+
+    protein_sequence_ns = str(protein_sequence.replace(' ', ''))
+    protein = ProteinAnalysis(protein_sequence_ns.upper())
+    reference_points = {
+        'molecular_weight': protein.molecular_weight(),
+        'aromaticity': protein.aromaticity(),
+        'instability_index': protein.instability_index(),
+        'flexibility': protein.flexibility(),
+        'isoelectric_point': protein.isoelectric_point(),
+        'mono isotopic': protein.monoisotopic(),
+        'gravy': protein.gravy(),
+        'secondary_structure_inclination': {
+            'Helix_propensity': protein.secondary_structure_fraction()[0],
+            'Turn_propensity': protein.secondary_structure_fraction()[1],
+            'Sheet_propensity': protein.secondary_structure_fraction()[2]
+        }
+
+    }
+    return reference_points
+
 
 def get_model_structure(
     raw_attention: tuple[torch.Tensor, ...]
