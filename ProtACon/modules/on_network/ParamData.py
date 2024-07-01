952c4305bc663ec11e0b3bf4aa429c627a3115cb
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-07-01 14:22:25 +0200

    initializaiton of the scripts of on_network module

---
+++ ProtACon/modules/on_network/ParamData.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__email__ = 'renatoeliasy@gmail.com'
+__author__ = 'Renato Eliasy'
+
+'''here a dataset to mimic the module ProtParamData of biopython
+also a new expression for flexibility calculus, since it doen't work as it should be'''
