1dad377b389fb1648d130ef6c357b2100140221f
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-07-11 12:32:28 +0200

    added the function to collect partition's quality results

--- ProtACon/modules/on_network/networks_analysis.py
+++ ProtACon/modules/on_network/networks_analysis.py
@@ -3,3 +3,36 @@
 
 __email__ = 'renatoeliasy@gmail.com'
 __author__ = 'Renato Eliasy'
+
+import numpy as np
+
+'''
+this script analyze the amminoacids in the protein, it also enhance some selected features
+through colors
+
+'''
+
+
+def collect_results_about_partitions(homogeneity: float,
+                                     completeness: float
+                                     ) -> tuple[float, ...]:
+    '''
+    starting from homogeneity and completeness it returns a triplet tuple to collect
+    and compute calculation about the V-measure:
+
+    Parameters:
+    ----------
+    homogeneity: float
+        the homogeneity score
+    completeness: float
+        the completeness score
+
+    Returns:
+    -------
+    partitions_results : tuple[float,...]
+        the V-measure, the homogeneity and the completeness
+    '''
+    V_measure = (homogeneity*completeness)/np.sum(homogeneity, completeness)
+    V_measure = np.round(V_measure, 2)
+    partitions_results = (V_measure, homogeneity, completeness)
+    return partitions_results
