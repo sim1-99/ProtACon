0afb33541e4e82ec2c6be860e758d0a27c91c449
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-07-14 18:18:47 +0200

    adde doc information on dataframe used for network analysis

--- ProtACon/modules/on_network/networks_analysis.py
+++ ProtACon/modules/on_network/networks_analysis.py
@@ -9,7 +9,13 @@ import numpy as np
 '''
 this script analyze the amminoacids in the protein, it also enhance some selected features
 through colors
-
+the dataframe used here has these basics columns:
+       'AA_Name', 'AA_KD_hydrophobicity', 'AA_Volume', 'AA_Charge', 'AA_isoPH',
+       'AA_Charge_Density', 'AA_Rcharge_density', 'AA_Xcoords', 'AA_Ycoords',
+       'AA_Zcoords', 'AA_self_Flexibility', 'AA_HW_hydrophylicity',
+       'AA_JA_in->out_E.transfer', 'AA_EM_surface.accessibility',
+       'AA_local_flexibility', 'aromaticity', 'web_groups',
+       'secondary_structure', 'vitality', 'AA_pos'
 '''
 
 
