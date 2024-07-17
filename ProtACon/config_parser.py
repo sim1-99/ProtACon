aaa627209ebbe7c705ae0515ed4df13c42a133c9
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-07-17 11:21:31 +0200

    added the cutoff fro instability index function in config files

--- ProtACon/config_parser.py
+++ ProtACon/config_parser.py
@@ -50,7 +50,11 @@ class Config:
             "DISTANCE_CUTOFF": float(
                 self.config.get("cutoffs", "DISTANCE_CUTOFF")),
             "POSITION_CUTOFF": int(
-                self.config.get("cutoffs", "POSITION_CUTOFF"))
+                self.config.get("cutoffs", "POSITION_CUTOFF")),
+            "INSTABILITY_CUTOFF": float(
+                self.config.get("cutoffs", "INSTABILITY_CUTOFF")),
+            "STABILITY_CUTOFF": float(
+                self.config.get("cutoffs", "STABILITY_CUTOFF"))
         }
 
     def get_paths(
