22ad609dc2087f062d112be463ad0628fd0a03bf
Simone Chiarella <simonechiarella99@gmail.com>
2024-09-18 01:03:20 +0200

    Change how avg quantities are computed - II
    Switch from the previous approach - sum the quantities together and then divide by the number of proteins - to one more correct - quantities are computed in the end from the sum of att_to_am_ac of the different proteins.
    This approach must still be implemented for avg_head_att_align and avg_layer_att_align.

--- ProtACon/average_on_set.py
+++ ProtACon/average_on_set.py
@@ -21,16 +21,16 @@ import pandas as pd
 import torch
 
 from ProtACon import config_parser
+from ProtACon.modules.attention import compute_attention_similarity
 from ProtACon.modules.utils import Loading
 
 
 def main(
-    sum_rel_att_to_am_ac: torch.Tensor,
-    sum_weight_att_to_am_ac: torch.Tensor,
-    sum_att_sim_df: pd.DataFrame,
-    sum_head_att_align_list: np.ndarray,
-    sum_layer_att_align_list: np.ndarray,
-    number_of_samples: int,
+    sum_att_head_sum: torch.Tensor,
+    sum_att_to_am_ac: torch.Tensor,
+    sum_head_att_align: np.ndarray,
+    sum_layer_att_align: np.ndarray,
+    sum_amino_acid_df: pd.DataFrame,
 ) -> tuple[
     torch.Tensor,
     torch.Tensor,
@@ -43,20 +43,15 @@ def main(
 
     Parameters
     ----------
-    sum_rel_att_to_am_ac : torch.Tensor
-        Tensor with shape (number_of_amino_acids, number_of_layers,
+    sum_att_head_sum : torch.Tensor
+        Tensor with shape (number_of_layers, number_of_heads), resulting from
+        the sum over the set of proteins of the sum over all the values of each
+        attention matrix.
+    sum_att_to_am_ac : torch.Tensor
+        Tensor with shape (len(sum_amino_acid_df), number_of_layers,
         number_of_heads), storing the sum over the set of proteins of the
-        relative attention in percentage given to each amino acid by each
-        attention head; "rel" (relative) means that the values of attention
-        given by one head to one amino acid are divided by the total value of
-        attention of that head.
-    sum_weight_att_to_am_ac : torch.Tensor
-        The tensor resulting from weighting sum_rel_att_to_amino_acids by the
-        number of occurrences of the corresponding amino acid.
-    sum_att_sim_df : pd.DataFrame
-        The sum over the set of proteins of the attention similarity data
-        frames between each couple of amino acids for each peptide chain.
-    sum_head_att_align_list : np.ndarray
+        absolute attention given to each amino acid by each attention head.
+    sum_head_att_align : np.ndarray
         The sum over the set of proteins of the arrays, each one with shape
         (number_of_layers, number_of_heads), storing how much attention aligns
         with indicator_function for each attention matrix.
@@ -65,8 +60,9 @@ def main(
         (number_of_layers), storing how much attention aligns with
         indicator_function for each average attention matrix computed
         independently over each layer.
-    number_of_samples : int
-        The number of proteins in the set.
+    sum_amino_acid_df : pd.DataFrame
+        The data frame containing the information about all the amino acids
+        in the set of proteins.
 
     Returns
     -------
@@ -74,9 +70,7 @@ def main(
         The percentage of attention given to each amino acid by each attention
         head, averaged over the whole protein set.
     avg_PW_att_to_amino_acids : torch.Tensor
-        The percentage of weighted attention given to each amino acid by each
-        attention head, averaged over the whole protein set.
-    avg_att_sim_arr : np.ndarray
+    avg_att_sim_df : pd.DataFrame
         The attention similarity averaged over the whole protein set.
     avg_head_att_align : np.ndarray
         The head attention alignment averaged over the whole protein set.
@@ -91,25 +85,43 @@ def main(
     file_dir = Path(__file__).resolve().parents[1]/file_folder
 
     with Loading("Saving average percentage of attention to amino acids"):
-        avg_P_att_to_amino_acids = sum_rel_att_to_am_ac/number_of_samples*100
+        avg_P_att_to_am_ac = 100*torch.div(
+            sum_att_to_am_ac,
+            torch.sum(sum_att_to_am_ac),
+        )
         torch.save(
-            avg_P_att_to_amino_acids, file_dir/"avg_P_att_to_amino_acids.pt"
+            avg_P_att_to_am_ac, file_dir/"avg_P_att_to_amino_acids.pt"
         )
 
     with Loading(
         "Saving average percentage of weighted attention to amino acids"
     ):
-        avg_PW_att_to_amino_acids = \
-            sum_weight_att_to_am_ac/number_of_samples*100
+        occurrences = torch.tensor(
+            sum_amino_acid_df["Occurrences"].to_list()
+        )
+        avg_W_att_to_am_ac = torch.div(
+            sum_att_to_am_ac, occurrences.unsqueeze(1).unsqueeze(1)
+        )
+        avg_PW_att_to_am_ac = 100*torch.div(
+            avg_W_att_to_am_ac,
+            torch.sum(avg_W_att_to_am_ac),
+        )
         torch.save(
-            avg_PW_att_to_amino_acids, file_dir/"avg_PW_att_to_amino_acids.pt"
+            avg_PW_att_to_am_ac, file_dir/"avg_PW_att_to_amino_acids.pt"
         )
 
     with Loading("Saving average attention similarity"):
-        avg_att_sim_df = sum_att_sim_df.div(number_of_samples)
+        avg_att_sim_df = compute_attention_similarity(
+            sum_att_to_am_ac, sum_amino_acid_df["Amino Acid"].to_list()
+        )
         # set diagonal to NaN
         avg_att_sim_arr = avg_att_sim_df.to_numpy()
         np.fill_diagonal(avg_att_sim_arr, np.nan)
+        avg_att_sim_df = pd.DataFrame(
+            data=avg_att_sim_arr,
+            index=avg_att_sim_df.index,
+            columns=avg_att_sim_df.columns,
+        )
         avg_att_sim_df.to_csv(
             file_dir/"attention_sim_df.csv", index=True, sep=';'
         )
@@ -123,9 +135,9 @@ def main(
         np.save(file_dir/"avg_layer_att_align.npy", avg_layer_att_align)
 
     return (
-        avg_P_att_to_amino_acids,
-        avg_PW_att_to_amino_acids,
-        avg_att_sim_arr,
+        avg_P_att_to_am_ac,
+        avg_PW_att_to_am_ac,
+        avg_att_sim_df,
         avg_head_att_align,
         avg_layer_att_align,
     )
