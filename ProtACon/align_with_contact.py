3df438049df0b227c301092e525aae18d4f14b88
Simone Chiarella <simonechiarella99@gmail.com>
2024-09-18 00:30:14 +0200

    Make variable names shorter

--- ProtACon/align_with_contact.py
+++ ProtACon/align_with_contact.py
@@ -5,9 +5,8 @@ Author: S. Chiarella
 
 This script combines other scripts for the computation of the attention
 alignment of the contact map of one protein. Other meaningful quantities, such
-as pairwise attention similarity, are computed too. In case of a set of
-proteins, those quantities can be averaged over it. The user can also choose if
-to plot and save all the plots of every single protein in the set.
+as pairwise attention similarity, are computed too. The user can also choose if
+to plot those quantities for every single protein in the set.
 
 """
 from __future__ import annotations
@@ -31,7 +30,7 @@ def main(
     attention: tuple[torch.Tensor, ...],
     CA_Atoms: tuple[CA_Atom, ...],
     chain_amino_acids: list[str],
-    attention_to_amino_acids: torch.Tensor,
+    att_to_am_ac: torch.Tensor,
     seq_ID: str,
     save_opt: str,
 ) -> tuple[
@@ -51,10 +50,10 @@ def main(
     CA_Atoms: tuple[CA_Atom, ...]
     chain_amino_acids : list[str]
         The single letter codes of the amino acid types in the peptide chain.
-    attention_to_amino_acids : torch.Tensor
-        Tensor with shape (number_of_amino_acids, number_of_layers,
-        number_of_heads), storing the absolute attention given to each amino
-        acid by each attention head.
+    att_to_am_ac : torch.Tensor
+        Tensor with shape (len(all_amino_acids), number_of_layers,
+        number_of_heads), storing the absolute attention given to each type of
+        amino acid by each attention head.
     seq_ID : str
         The alphanumerical code representing uniquely the peptide chain.
     save_opt : str
@@ -65,10 +64,10 @@ def main(
     -------
     head_att_align : np.ndarray
         Array with shape (number_of_layers, number_of_heads), storing how much
-        attention aligns with indicator_function for each attention masks.
+        attention aligns with indicator_function for each attention matrices.
     layer_att_align : np.ndarray
         Array with shape (number_of_layers), storing how much attention aligns
-        with indicator_function for each average attention mask computed
+        with indicator_function for each average attention matrix computed
         independently over each layer.
 
     """
@@ -84,18 +83,18 @@ def main(
         CA_Atoms
     )
 
-    att_sim_df, attention_avgs, attention_align = process_attention.main(
-        attention, attention_to_amino_acids, binary_contact_map,
-        chain_amino_acids
-    )
+    att_sim_df, att_avgs, head_att_align, layer_att_align = \
+        process_attention.main(
+            attention, att_to_am_ac, binary_contact_map, chain_amino_acids
+        )
 
     if save_opt in save_if:
         seq_dir = plot_dir/seq_ID
         seq_dir.mkdir(parents=True, exist_ok=True)
         plotting.plot_on_chain(
             distance_map, norm_contact_map, binary_contact_map, attention,
-            attention_avgs, attention_to_amino_acids, att_sim_df,
-            attention_align, seq_dir, chain_amino_acids
+            att_avgs, att_to_am_ac, att_sim_df, head_att_align,
+            layer_att_align, seq_dir, chain_amino_acids
         )
 
     return (
