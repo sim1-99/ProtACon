Fix wrong broadcasting

ProtACon/manage_tot_ds.py
@@ -75,7 +75,7 @@ def create(
 
     tot_att_head_sum = torch.zeros(n_layers, n_heads)
     tot_att_to_aa = torch.zeros(len(all_amino_acids), n_layers, n_heads)
-    tot_head_att_align = np.zeros(n_layers, n_heads)
+    tot_head_att_align = np.zeros((n_layers, n_heads))
     tot_layer_att_align = np.zeros(n_layers)
 
     return (

ProtACon/modules/attention.py
@@ -120,7 +120,7 @@ def compute_attention_alignment(
 
     if len(attention[0].size()) == 3:
         n_heads, n_layers = get_model_structure(attention)
-        attention_alignment = np.empty(n_layers, n_heads)
+        attention_alignment = np.empty((n_layers, n_heads))
         for layer_idx, layer in enumerate(attention):
             for head_idx, head in enumerate(layer):
                 head = head.numpy()