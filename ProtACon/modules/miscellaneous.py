75d4bf5fce2944e7322c5e4158f3a5a77ae98d5d
Simone Chiarella <simonechiarella99@gmail.com>
2024-10-11 09:59:01 +0200

    Suppress warning

--- ProtACon/modules/miscellaneous.py
+++ ProtACon/modules/miscellaneous.py
@@ -967,10 +967,14 @@ def load_model(
 
     """
     model = BertModel.from_pretrained(
-        model_name, output_attentions=True, attn_implementation="eager"
+        model_name,
+        output_attentions=True,
+        attn_implementation="eager",
     )
     tokenizer = BertTokenizer.from_pretrained(
-        model_name, do_lower_case=False
+        model_name,
+        do_lower_case=False,
+        clean_up_tokenization_spaces=True,
     )
 
     return (
