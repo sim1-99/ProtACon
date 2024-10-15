060d76a33fe96f60fa5edc547650d1ca9e3fd6a0
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-10-15 17:12:52 +0200

    finalize the summary of graph for louvain communities extraction

--- ProtACon/modules/on_network/summarize_results_for_main.py
+++ ProtACon/modules/on_network/summarize_results_for_main.py
@@ -55,13 +55,14 @@ def get_kmeans_results(
 
 def prepare_complete_graph_nx(CA_Atoms: tuple[CA_Atom, ...],
                               binary_map: np.ndarray | None
-                              ) -> nx.Graph:
+                              ) -> tuple[nx.Graph, float]:
     '''
     from the CA_Atoms list it's in need:
     - the AA_dataframe
     - the the instability value from DIWV dict
     - the distancies between AAs
     - the bool to see if they are in contact or not
+    it give back the complete graph and the resolution for the louvain communities computing
     '''
     node_name_for_Graph = Collect_and_structure_data.generate_index_df(
         CA_Atoms=CA_Atoms)
@@ -69,6 +70,7 @@ def prepare_complete_graph_nx(CA_Atoms: tuple[CA_Atom, ...],
         CA_Atoms=CA_Atoms), index_str=node_name_for_Graph, columns_str=node_name_for_Graph)
     distance_df = Collect_and_structure_data.get_dataframe_from_nparray(base_map=generate_distance_map(
         CA_Atoms=CA_Atoms), index_str=node_name_for_Graph, columns_str=node_name_for_Graph)
+
     if binary_map == None:
         list_of_edges = []
         for i, AA_i in enumerate(node_name_for_Graph):
@@ -78,28 +80,89 @@ def prepare_complete_graph_nx(CA_Atoms: tuple[CA_Atom, ...],
     else:
         list_of_edges, _ = Collect_and_structure_data.get_list_of_edges(
             base_map=binary_map, CA_Atoms=CA_Atoms)
-        weights_for_edges = Collect_and_structure_data.get_weight_for_edges(
-            list_of_edges=list_of_edges,
-            base_df=distance_df,
-            instability_df=instability_df)
-    nx_graph = Collect_and_structure_data.get_the_Graph_network(
+
+    weights_for_edges = Collect_and_structure_data.get_weight_for_edges(
+        list_of_edges=list_of_edges,
+        base_df=distance_df,
+        instability_df=instability_df)
+    nx_graph, resolution = Collect_and_structure_data.get_the_Graph_network(
         CA_Atoms=CA_Atoms, edges_weight_list=weights_for_edges)
-    return nx_graph
 
-
-pass
+    return (nx_graph, resolution)
 
 
 # Define the results for louvain
-def get_louvain_results():
+def get_louvain_results(CA_Atoms: tuple[CA_Atom, ...],
+                        base_Graph: nx.Graph,
+                        resolution: float,
+                        threshold: float = None,  # optional
+                        threshold_type: str = 'zero',  # optional
+                        # optional
+                        edge_weights_combination: tuple[float,
+                                                        float, float] | dict = False
+                        ) -> tuple[nx.Graph, tuple[int, ...]]:
     '''
     It give the results of the louvain analysis
+    Parameters:
+    ----------
+    CA_Atoms: tuple[CA_Atom,...]
+        the tuple of the CA_Atom objects
+    base_Graph: nx.Graph
+        the base graph to be used for the louvain analysis
+    edge_weights_combination: tuple[float,float,float] | dict
+        the weights to be used on the edge for the community modulation:
+        if a dict was given it has to have the keys: {'contact_in_sequence' : float
+                                                      'lenght' : float
+                                                      'instability' : float}
+    threshold: float
+        the threshold to be used for the proximity graph, to filter the edge considering a certain threshold
+        it can be Not specified if the map used for the base graph it was already filtred
+    Threshold_type : str
+        the type of threshold to be used, it can be 'zero' or 'abs', zero is not included if 'zero' is selected 
     Returns:
     -------
     tuple[pd.DataFrame, tuple[int,...]]
         the updated dataframe
         the louvain_labels
     '''
-    # final generate the dataframe with louvain communities
-    # generate necessary object to use to create the base network
-    pass
+
+    # assessing the weight on edge, respecting the type of data
+    # of edge_weights_combination:
+    if not edge_weights_combination:
+        edge_weights = networks_analysis.weight_on_edge()
+    elif isinstance(edge_weights_combination, tuple):
+        edge_weights = networks_analysis.weight_on_edge(
+            contact=edge_weights_combination[0],
+            lenght=edge_weights_combination[1],
+            stability=edge_weights_combination[2]
+        )
+    elif isinstance(edge_weights_combination, dict):
+        for k in ['contact_in_sequence', 'lenght', 'instability']:
+            if k not in edge_weights_combination.keys():
+                raise KeyError(
+                    f"the key {k} is missing in the edge_weights_combination dict")
+
+        edge_weights = edge_weights_combination
+    # add 'weight_combination' as attribute to node on graph
+    new_graph = networks_analysis.add_weight_combination(G=base_Graph,
+                                                         weight_to_edge=edge_weights)
+
+    # if a threshold was given, filter the graph:
+    if threshold != None:
+        for source, target in new_graph.edges:
+            if threshold_type.lower() == 'abs':
+                if not abs(new_graph.get_edge_data(source, target)['weight_combination']) < threshold:
+                    new_graph.remove_edge(source, target)
+            elif threshold_type.lower() == 'zero':
+                if not (0 < new_graph.get_edge_data(source, target)['weight_combination'] < threshold):
+                    new_graph.remove_edge(source, target)
+            else:
+                raise ValueError('The threshold type is not recognized')
+
+    # add the louvain community attribute to each node in the graph:
+    final_Graph, louvain_communities = networks_analysis.add_louvain_community_attribute(G=new_graph,
+                                                                                         weight_of_edge='weight_combination',
+                                                                                         resolution=resolution)
+
+    # finally return both the graph with weight_combination attributes on edges and louvain_community on nodes:
+    return (final_Graph, louvain_communities)
