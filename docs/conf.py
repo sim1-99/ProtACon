874ec0761620d9880654bc89601db6b1a699592f
sim1-99 <simonechiarella99@gmail.com>
2024-12-06 19:30:05 +0100

    Add autodoc extension

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Simone Chiarella'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
 ]
 
